65b6b96f1539b0e6933dd08fc045c0cfb1acd670
Freya Murphy <freya@freyacat.org>
2026-08-10 11:12:07 -0400

    remove foojay-resolver gradle plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,8 +8,6 @@ pluginManagement {
     }
 }
 
-plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" }
-
 rootProject.name = "KenshinsHideAndSeek"
 
 include("core", "bukkit", "mod", "fabric", "neoforge")
